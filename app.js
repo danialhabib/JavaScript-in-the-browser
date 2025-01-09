aeef8366f7da7263f0a053d04b7f46ca80344b5e
danialhabib <danialads12@gmail.com>
2025-01-09 10:13:19 +0500

    mofified index.html and app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input");
+
+// btnTranslate.addEventListener("click", function clickEventHandler() {
+//     console.log("clicked!")
+// })
+
+function clickHandler() {
+    console.log("clicked!")
+    console.log("input", txtInput.value);
+};
+
+btnTranslate.addEventListener("click", clickHandler) 
+
