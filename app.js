5beef9baa2348c6bb24d9929b15b255747355b1e
danialhabib <danialads12@gmail.com>
2025-01-09 10:48:42 +0500

    input output processing done

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-var btnTranslate = document.querySelector("#btn-translate")
+var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outPutDiv = document.querySelector("#output");
 
 // btnTranslate.addEventListener("click", function clickEventHandler() {
 //     console.log("clicked!")
 // })
 
+// outputDiv.innerText = "Tanay Pratap"
+
+console.log(outPutDiv)
+
 function clickHandler() {
-    console.log("clicked!")
-    console.log("input", txtInput.value);
+    // console.log("clicked!")
+    // console.log("input", txtInput.value);
+    outPutDiv.innerText = "da da da a da ad  " + txtInput.value; 
 };
 
 btnTranslate.addEventListener("click", clickHandler) 
